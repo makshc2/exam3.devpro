a11af860e7d286a7c6738e2689d6385656d2f23c
makshc2 <makshc2@gmail.com>
2017-11-22 20:30:35 +0200

    test

---
+++ index.js
@@ -0,0 +1,24 @@
+const newsFeed = {
+    _articles: [],
+    _isBusy: false,
+    init(articles) {},
+    addArticle(article, cb) {},
+    removeArticle(article, cb) {},
+    find(functor, cb) {},
+    query(queryString) {}
+};
+
+
+
+
+const article = {
+    title: 'required',
+    content: 'required'
+}
+
+
+module.exports = {
+    firstName: 'Maksim',
+    lastName: 'Shevjakov',
+    task: ProjectModule
+}
